96f5a21f819db249d4ceb20546a145147f3f0a22
Konstantin <konstantinhirschberger@web.de>
2023-12-05 23:22:18 +0100

    Tried optimizations failed ):

--- AoC_2023/src/Day5/Main.java
+++ AoC_2023/src/Day5/Main.java
@@ -192,6 +192,7 @@ public class Main {
     public static void partTwo(){
         {
             String inputRaw = loadFile("inputFive");
+            inputRaw = inputRaw.replaceAll("\r\n","\n");
 
             String seedsline = inputRaw.substring(7,inputRaw.indexOf("\n"));
             seedsline += " ;";
@@ -204,6 +205,7 @@ public class Main {
                 long initialSeedNumber = Long.parseLong(seedsline.substring(0,seedsline.indexOf(" ")));
                 seedsInitial.add(initialSeedNumber);
                 seedsline = seedsline.substring(seedsline.indexOf(" ")+1);
+                //System.out.println(seedsline);
                 long seedsRange = Long.parseLong(seedsline.substring(0,seedsline.indexOf(" ")));
                 seedsEnd.add(seedsRange);
                 seedsline = seedsline.substring(seedsline.indexOf(" ")+1);
@@ -215,7 +217,8 @@ public class Main {
 
             String seedToSoilMap = inputRaw.substring(inputRaw.indexOf("seed-to-soil map:"),inputRaw.indexOf("soil-to-fertilizer map:"));
             seedToSoilMap = seedToSoilMap.substring(seedToSoilMap.indexOf("\n")+1);
-            ArrayList<Long> seedToSoil = new ArrayList<>();
+            ArrayList<Long> seedToSoilInitial = new ArrayList<>();
+            ArrayList<Long> seedToSoilEnd = new ArrayList<>();
             //for (long i: seeds) seedToSoil.add(i);
             String lines[] = seedToSoilMap.split("\n");
             for (String line : lines){
@@ -229,28 +232,24 @@ public class Main {
 
                 for(int i = 0; i<seedsInitial.size();i++){
                     long currentIntialSeed = seedsInitial.get(i);
-                    long currenEndSeed = seedsInitial.get(i)+seedsEnd.get(i);
+                    long currenEndSeed = seedsInitial.get(i)+seedsEnd.get(i)-1;
                     if(currentIntialSeed>sourceEnd)continue;
                     if(currenEndSeed<sourceRangeStart)continue;
                     System.out.println("Intital Seed: "+currentIntialSeed + " EndSeed: "+currenEndSeed);
                     System.out.println("Source Range Start: "+sourceRangeStart + " SourceEnd:" + sourceEnd);
-                    for(long j=sourceRangeStart;sourceRangeStart<sourceEnd;j++){
-                        if(j>currenEndSeed)break;
-                        seedToSoil.add(j);
-                    }
+                    if(currentIntialSeed>sourceRangeStart)seedToSoilInitial.add(currentIntialSeed);
+                    else seedToSoilInitial.add(sourceRangeStart);
+                    if(currenEndSeed>sourceEnd) seedToSoilEnd.add(sourceEnd);
+                    else seedToSoilEnd.add(currenEndSeed);
                 }
             }
-
-            for(long i: seedToSoil){
-                System.out.println(i);
-            }
+            for(long i: seedToSoilInitial) System.out.println(i);
 
 
-            /*
             String soilToFertilizerMap = inputRaw.substring(inputRaw.indexOf("soil-to-fertilizer map:"),inputRaw.indexOf("fertilizer-to-water map:"));
             soilToFertilizerMap = soilToFertilizerMap.substring(soilToFertilizerMap.indexOf("\n")+1);
-            ArrayList<Long> soilToFertilizer = new ArrayList<>();
-            for (long i: seedToSoil) soilToFertilizer.add(i);
+            ArrayList<Long> soilToFertilizerInitial = new ArrayList<>();
+            ArrayList<Long> soilToFertilizerEnd = new ArrayList<>();
             lines = soilToFertilizerMap.split("\n");
             for (String line : lines){
                 long destinationRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
@@ -258,19 +257,27 @@ public class Main {
                 long sourceRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
                 line = line.substring(line.indexOf(" ")+1);
                 long rangeLength = Long.parseLong(line);
+                long sourceEnd = sourceRangeStart + rangeLength;
 
-                for(int i = 0; i<soilToFertilizer.size();i++){
-                    long soil = seedToSoil.get(i);
-                    if(sourceRangeStart <= soil && soil <= sourceRangeStart+rangeLength){
-                        soilToFertilizer.set(soilToFertilizer.indexOf(soil),soil+(destinationRangeStart-sourceRangeStart));
-                    }
+                for(int i = 0; i<seedToSoilInitial.size();i++){
+                    long currentIntial = seedToSoilInitial.get(i);
+                    long currenEnd = seedToSoilInitial.get(i)+seedsEnd.get(i);
+
+                    if(currentIntial>sourceEnd)continue;
+                    if(currenEnd<sourceRangeStart)continue;
+
+                    if(currentIntial>sourceRangeStart)soilToFertilizerInitial.add(currentIntial);
+                    else soilToFertilizerInitial.add(sourceRangeStart);
+                    if(currenEnd>sourceEnd) soilToFertilizerEnd.add(sourceEnd);
+                    else soilToFertilizerEnd.add(currenEnd);
                 }
             }
+            for(long i: soilToFertilizerInitial) System.out.println(i);
 
             String fertilizerToWaterMap = inputRaw.substring(inputRaw.indexOf("fertilizer-to-water map:"),inputRaw.indexOf("water-to-light map:"));
             fertilizerToWaterMap = fertilizerToWaterMap.substring(fertilizerToWaterMap.indexOf("\n")+1);
-            ArrayList<Long> fertilizerToWater = new ArrayList<>();
-            for (long i: soilToFertilizer) fertilizerToWater.add(i);
+            ArrayList<Long> fertilizerToWaterInitial = new ArrayList<>();
+            ArrayList<Long> fertilizerToWaterEnd = new ArrayList<>();
             lines = fertilizerToWaterMap.split("\n");
             for (String line : lines){
                 long destinationRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
@@ -278,18 +285,26 @@ public class Main {
                 long sourceRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
                 line = line.substring(line.indexOf(" ")+1);
                 long rangeLength = Long.parseLong(line);
-                for(int i = 0; i<fertilizerToWater.size();i++){
-                    long fertilizer = soilToFertilizer.get(i);
-                    if(sourceRangeStart <= fertilizer && fertilizer <= sourceRangeStart+rangeLength){
-                        fertilizerToWater.set(fertilizerToWater.indexOf(fertilizer),fertilizer+(destinationRangeStart-sourceRangeStart));
-                    }
+                long sourceEnd = sourceRangeStart + rangeLength;
+
+                for(int i = 0; i<soilToFertilizerInitial.size();i++){
+                    long currentIntial = soilToFertilizerInitial.get(i);
+                    long currenEnd = soilToFertilizerEnd.get(i)+seedsEnd.get(i);
+
+                    if(currentIntial>sourceEnd)continue;
+                    if(currenEnd<sourceRangeStart)continue;
+
+                    if(currentIntial>sourceRangeStart)fertilizerToWaterInitial.add(currentIntial);
+                    else fertilizerToWaterInitial.add(sourceRangeStart);
+                    if(currenEnd>sourceEnd) fertilizerToWaterEnd.add(sourceEnd);
+                    else fertilizerToWaterEnd.add(currenEnd);
                 }
             }
 
             String waterToLightMap = inputRaw.substring(inputRaw.indexOf("water-to-light map:"),inputRaw.indexOf("light-to-temperature map:"));
             waterToLightMap = waterToLightMap.substring(waterToLightMap.indexOf("\n")+1);
-            ArrayList<Long> waterToLight = new ArrayList<>();
-            for (long i: fertilizerToWater) waterToLight.add(i);
+            ArrayList<Long> waterToLightInitial = new ArrayList<>();
+            ArrayList<Long> waterToLightEnd = new ArrayList<>();
             lines = waterToLightMap.split("\n");
             for (String line : lines){
                 long destinationRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
@@ -297,18 +312,26 @@ public class Main {
                 long sourceRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
                 line = line.substring(line.indexOf(" ")+1);
                 long rangeLength = Long.parseLong(line);
-                for(int i = 0; i<waterToLight.size();i++){
-                    long water = fertilizerToWater.get(i);
-                    if(sourceRangeStart <= water && water <= sourceRangeStart+rangeLength){
-                        waterToLight.set(waterToLight.indexOf(water),water+(destinationRangeStart-sourceRangeStart));
-                    }
+                long sourceEnd = sourceRangeStart + rangeLength;
+
+                for(int i = 0; i<fertilizerToWaterInitial.size();i++){
+                    long currentIntial = fertilizerToWaterInitial.get(i);
+                    long currenEnd = fertilizerToWaterEnd.get(i)+seedsEnd.get(i);
+
+                    if(currentIntial>sourceEnd)continue;
+                    if(currenEnd<sourceRangeStart)continue;
+
+                    if(currentIntial>sourceRangeStart)waterToLightInitial.add(currentIntial);
+                    else waterToLightInitial.add(sourceRangeStart);
+                    if(currenEnd>sourceEnd) waterToLightEnd.add(sourceEnd);
+                    else waterToLightEnd.add(currenEnd);
                 }
             }
 
             String lightToTemperatureMap = inputRaw.substring(inputRaw.indexOf("light-to-temperature map:"),inputRaw.indexOf("temperature-to-humidity map:"));
             lightToTemperatureMap = lightToTemperatureMap.substring(lightToTemperatureMap.indexOf("\n")+1);
-            ArrayList<Long> lightToTemperature = new ArrayList<>();
-            for (long i: waterToLight) lightToTemperature.add(i);
+            ArrayList<Long> lightToTemperatureInitial = new ArrayList<>();
+            ArrayList<Long> lightToTemperatureEnd = new ArrayList<>();
             lines = lightToTemperatureMap.split("\n");
             for (String line : lines){
                 long destinationRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
@@ -316,11 +339,19 @@ public class Main {
                 long sourceRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
                 line = line.substring(line.indexOf(" ")+1);
                 long rangeLength = Long.parseLong(line);
-                for(int i = 0; i<lightToTemperature.size();i++){
-                    long temperature = waterToLight.get(i);
-                    if(sourceRangeStart <= temperature && temperature <= sourceRangeStart+rangeLength){
-                        lightToTemperature.set(lightToTemperature.indexOf(temperature),temperature+(destinationRangeStart-sourceRangeStart));
-                    }
+                long sourceEnd = sourceRangeStart + rangeLength;
+
+                for(int i = 0; i<waterToLightInitial.size();i++){
+                    long currentIntial = waterToLightInitial.get(i);
+                    long currenEnd = waterToLightEnd.get(i)+seedsEnd.get(i);
+
+                    if(currentIntial>sourceEnd)continue;
+                    if(currenEnd<sourceRangeStart)continue;
+
+                    if(currentIntial>sourceRangeStart)lightToTemperatureInitial.add(currentIntial);
+                    else lightToTemperatureInitial.add(sourceRangeStart);
+                    if(currenEnd>sourceEnd) lightToTemperatureEnd.add(sourceEnd);
+                    else lightToTemperatureEnd.add(currenEnd);
                 }
             }
 
@@ -328,8 +359,8 @@ public class Main {
 
             String temperatureToHumidityMap = inputRaw.substring(inputRaw.indexOf("temperature-to-humidity map:"),inputRaw.indexOf("humidity-to-location map:"));
             temperatureToHumidityMap = temperatureToHumidityMap.substring(temperatureToHumidityMap.indexOf("\n")+1);
-            ArrayList<Long> temperatureToHumidity = new ArrayList<>();
-            for (long i: lightToTemperature) temperatureToHumidity.add(i);
+            ArrayList<Long> temperatureToHumidityInitial = new ArrayList<>();
+            ArrayList<Long> temperatureToHumidityEnd = new ArrayList<>();
             lines = temperatureToHumidityMap.split("\n");
             for (String line : lines){
                 long destinationRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
@@ -337,18 +368,26 @@ public class Main {
                 long sourceRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
                 line = line.substring(line.indexOf(" ")+1);
                 long rangeLength = Long.parseLong(line);
-                for(int i = 0; i<temperatureToHumidity.size();i++){
-                    long humidity = lightToTemperature.get(i);
-                    if(sourceRangeStart <= humidity && humidity <= sourceRangeStart+rangeLength){
-                        temperatureToHumidity.set(temperatureToHumidity.indexOf(humidity),humidity+(destinationRangeStart-sourceRangeStart));
-                    }
+                long sourceEnd = sourceRangeStart + rangeLength;
+
+                for(int i = 0; i<fertilizerToWaterInitial.size();i++){
+                    long currentIntial = lightToTemperatureInitial.get(i);
+                    long currenEnd = lightToTemperatureEnd.get(i)+seedsEnd.get(i);
+
+                    if(currentIntial>sourceEnd)continue;
+                    if(currenEnd<sourceRangeStart)continue;
+
+                    if(currentIntial>sourceRangeStart)temperatureToHumidityInitial.add(currentIntial);
+                    else temperatureToHumidityInitial.add(sourceRangeStart);
+                    if(currenEnd>sourceEnd) temperatureToHumidityEnd.add(sourceEnd);
+                    else temperatureToHumidityEnd.add(currenEnd);
                 }
             }
 
             String humidityToLocationMap = inputRaw.substring(inputRaw.indexOf("humidity-to-location map:"));
             humidityToLocationMap = humidityToLocationMap.substring(humidityToLocationMap.indexOf("\n")+1);
-            ArrayList<Long> humidityToLocation = new ArrayList<>();
-            for (long i: temperatureToHumidity) humidityToLocation.add(i);
+            ArrayList<Long> humidityToLocationInitial = new ArrayList<>();
+            ArrayList<Long> humidityToLocationEnd = new ArrayList<>();
             lines = humidityToLocationMap.split("\n");
             for (String line : lines){
                 long destinationRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
@@ -356,24 +395,30 @@ public class Main {
                 long sourceRangeStart = Long.parseLong(line.substring(0,line.indexOf(" ")));
                 line = line.substring(line.indexOf(" ")+1);
                 long rangeLength = Long.parseLong(line);
-                for(int i = 0; i<humidityToLocation.size();i++){
-                    long location = temperatureToHumidity.get(i);
-                    if(sourceRangeStart <= location && location <= sourceRangeStart+rangeLength){
-                        humidityToLocation.set(humidityToLocation.indexOf(location),location+(destinationRangeStart-sourceRangeStart));
-                    }
+                long sourceEnd = sourceRangeStart + rangeLength;
+
+                for(int i = 0; i<temperatureToHumidityInitial.size();i++){
+                    long currentIntial = temperatureToHumidityInitial.get(i);
+                    long currenEnd = temperatureToHumidityEnd.get(i)+seedsEnd.get(i);
+
+                    if(currentIntial>sourceEnd)continue;
+                    if(currenEnd<sourceRangeStart)continue;
+
+                    if(currentIntial>sourceRangeStart)humidityToLocationInitial.add(currentIntial);
+                    else humidityToLocationInitial.add(sourceRangeStart);
+                    if(currenEnd>sourceEnd) humidityToLocationEnd.add(sourceEnd);
+                    else humidityToLocationEnd.add(currenEnd);
                 }
             }
 
 
 
-            long lowest = humidityToLocation.get(0);
-            for(long i: humidityToLocation){
+            long lowest = humidityToLocationInitial.get(0);
+            for(long i: humidityToLocationInitial){
                 if(i < lowest) lowest = i;
             }
             System.out.println();
             System.out.print(lowest);
-
-             */
 
         }
     }
